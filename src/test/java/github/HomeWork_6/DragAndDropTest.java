1c23f717cee773f0e7c83a9c12f3652876d1fd22
noredcat <anastasiaredcat@gmail.com>
2022-09-28 21:39:23 +0700

    added class DragAndDropTest with test

---
+++ src/test/java/github/HomeWork_6/DragAndDropTest.java
@@ -0,0 +1,28 @@
+package github.HomeWork_6;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+    @Test
+    void DragAndDropTest(){
+        Configuration.browserSize="1900x1000";
+        //открыть страницу the-internet.herokuapp.com/drag_and_drop
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        //Проверяем, что А находится на месте А, а B на месте B
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        //Меняем элементы местами
+        $("#column-a").dragAndDropTo($("#column-b"));
+        /*actions()
+                .clickAndHold($("#column-a"))
+                .moveToElement($("#column-b"))
+                .release();*/
+        //Проверяем, что А находится на месте B, а B на месте A
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+}
+}
--- src/test/java/github/HomeWork_6/TestGitHubMenuAndPricing.java
+++ src/test/java/github/HomeWork_6/TestGitHubMenuAndPricing.java
@@ -1,6 +1,7 @@
 package github.HomeWork_6;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -8,22 +9,24 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestGitHubMenuAndPricing {
-
-    @Test
-    void TestMenu(){
-        Configuration.browserSize="1900x1000";
+    @BeforeAll
+    static void setConfig(){
+        Configuration.browserSize = "1900x1000";
         //открыть страницу на github.com
         open("https://github.com");
-        //Навести машь на пункт меню Open Source
+    }
+    @Test
+    void testMenu() {
+        //Навести мышь на пункт меню Open Source
         $(".header-menu-wrapper").$(byText("Open Source")).hover();
         //кликнуть по пункту выпадающего списка Trending
         $(byText("Trending")).click();
         //Проверить, что на открывшейся странице заголовок содержит Trending
         $(".h1").shouldHave(text("Trending"));
     }
+
     @Test
-    void TestPricing(){
-        open("https://github.com");
+    void testPricing() {
         //Кликнуть на пункт меню Pricing
         $(".header-menu-wrapper").$(byText("Pricing")).hover().click();
         //Проверить, что на открывшейся странице заголовок содержит Get the complete developer platform.
